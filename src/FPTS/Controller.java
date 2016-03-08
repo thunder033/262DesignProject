15ae775ccf516e1109e92d73ad36631535e88f39
Gregory Rozmarynowycz <crashtest1@live.com>
2016-03-08 11:46:50 -0500

    create FPTS class

---
+++ src/FPTS/Controller.java
@@ -0,0 +1,12 @@
+package FPTS;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public abstract class Controller implements Observer {
+
+    public void update(Observable subject, Object obj)
+    {
+
+    }
+}
--- src/FPTS/Main.java
+++ src/FPTS/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package FPTS;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,8 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("FPTS/fpts.fxml"));
+        primaryStage.setTitle("ThunderForge FPTS");
+
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
