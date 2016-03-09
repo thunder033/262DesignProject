acca0058eaff8948be6d4ffffdcbc6f4288bcbd4
Alexander Kidd <alexanderkidd5@outlook.com>
2016-03-09 00:34:18 -0500

    Changelog 3/9/2016 12:33AM: Added an icon.  Not sure why XML namespace is not already registered, may have to use NetBeans only?

--- src/FPTS/FPTSApp.java
+++ src/FPTS/FPTSApp.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class FPTSApp extends Application {
@@ -11,8 +12,9 @@ public class FPTSApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("FPTS.fxml"));
-        primaryStage.setTitle("ThunderForge FPTS");
 
+        primaryStage.getIcons().add(new Image(this.getClass().getResourceAsStream("/assets/appIcon.png")));
+        primaryStage.setTitle("ThunderForge FPTS");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
