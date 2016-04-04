59c7ed8538d9b0939e128d81e2b555c4a4baa71a
Beetee14 <bjt2654@rit.edu>
2016-04-04 03:29:47 -0400

    Fixing Merge Errors

--- src/FPTS/Controllers/ImportHoldingsController.java
+++ src/FPTS/Controllers/ImportHoldingsController.java
@@ -1,33 +1,20 @@
 package FPTS.Controllers;
 
 import FPTS.Core.Controller;
-import FPTS.Core.Model;
-import FPTS.Models.Holding;
-import FPTS.Models.Portfolio;
-import FPTS.PortfolioImporter.CSVImporter;
-import FPTS.PortfolioImporter.CustomComboBoxFactory;
-import FPTS.PortfolioImporter.Importer;
-import FPTS.Views.AddHoldingView;
+import FPTS.PortfolioImporter.HoldingImportHandler;
+import FPTS.Views.ImportHoldingsView;
 import FPTS.Views.PortfolioView;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.TableCell;
-import javafx.scene.control.TableColumn;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.ComboBoxTableCell;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
 import javafx.stage.FileChooser;
-import javafx.util.Callback;
-import javafx.util.converter.DefaultStringConverter;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by traub_000 on 3/25/2016.
@@ -38,8 +25,10 @@ import java.util.Map;
 public class ImportHoldingsController extends Controller{
     @FXML private TableView holdingsPane;
     @FXML private Button importButton;
-    @FXML private javafx.scene.control.TableColumn duplicateCol;
+    @FXML private GridPane grid;
+    private ArrayList<ComboBox> duplicateBoxes;
     final FileChooser fileChooser = new FileChooser();
+    private GridPane newGrid = new GridPane();
 
     /**
      * This method stores information about the import file
@@ -48,37 +37,23 @@ public class ImportHoldingsController extends Controller{
      */
     public void fileSelected(ActionEvent event) {
         Path path = Paths.get(fileChooser.showOpenDialog(_app.getStage()).getPath());
-        Importer importer = new Importer(path);
-        importer.setStrategy(new CSVImporter());
-        ObservableList<Holding> holdings = FXCollections.observableArrayList(importer.importData().getHoldings());
-        holdingsPane.setItems(holdings);
-
-//        String[] choices = {"Merge", "Replace", "Ignore"};
-//        ObservableList items = FXCollections.observableArrayList(choices);
-//        CustomComboBoxFactory<Portfolio, String> cellFactory = new CustomComboBoxFactory<>();
-//        duplicateCol.setEditable(true);
-//        duplicateCol.setCellValueFactory(new PropertyValueFactory<>("value"));
-//        duplicateCol.setCellFactory(ComboBoxTableCell.<Portfolio, String>forTableColumn(items));
-
-
-        Map<String, String> holdingNames = new HashMap<>();
-        for(Holding hold: _portfolio.getHoldings()){
-            holdingNames.put(hold.getName(),hold.getType());
-        }
-        for(Holding newHolding: holdings) {
-            String newName = newHolding.getName();
-            if(holdingNames.containsKey(newName) && holdingNames.get(newName).equals(newHolding.getType())){
-
-            }
-        }
+        holdingsPane.setItems(HoldingImportHandler.getNewHoldings(path));
         importButton.setVisible(true);
-//        importer.importData().getHoldings().stream().forEach(this::addHolding);
-//        _portfolio.save();
-    }
 
-    private void addHolding(Holding holding){
-        _portfolio.addHolding(holding);
-        _app.getData().addInstance(Model.class.cast(holding));
+        HoldingImportHandler.determineConflicts(_portfolio);
+
+        boolean[] duplCAIndices = HoldingImportHandler.getDuplCAIndices();
+
+        ComboBox box = new ComboBox();
+        box.setVisible(false);
+        newGrid.addColumn(0, box);
+        for (boolean duplCAIndex : duplCAIndices) {
+            box = new ComboBox(HoldingImportHandler.getDuplOpts());
+            box.setPrefHeight(18);
+            box.setVisible(duplCAIndex);
+            newGrid.addColumn(0, box);
+        }
+        grid.add(newGrid, 6, 2);
     }
 
     /**
@@ -87,7 +62,9 @@ public class ImportHoldingsController extends Controller{
      * @param event unused
      */
     public void importHoldings(ActionEvent event){
-        
+        // Pass newGrid into below
+        HoldingImportHandler.importHoldings(_app, newGrid);
+        _app.loadView(new ImportHoldingsView(_app));
     }
 
     /**
--- src/FPTS/PortfolioImporter/InvalidChoiceException.java
+++ src/FPTS/PortfolioImporter/InvalidChoiceException.java
@@ -2,6 +2,12 @@ package FPTS.PortfolioImporter;
 
 /**
  * Created by traub_000 on 4/2/2016.
+ *
+ * Description: Error thrown when an invalid choice is selected for a
+ * duplicate holding
  */
-public class InvalidChoiceException {
+public class InvalidChoiceException extends Throwable {
+    public InvalidChoiceException(String message) {
+        super(message);
+    }
 }
