4ed10e520d240e7d912046803b4bee65acbc3f50
Beetee14 <bjt2654@rit.edu>
2016-04-04 02:27:09 -0400

    Merged from master

--- src/FPTS/Models/MarketEquity.java
+++ src/FPTS/Models/MarketEquity.java
@@ -1,11 +1,11 @@
 package FPTS.Models;
 
-        import FPTS.Core.Model;
-        import org.w3c.dom.Document;
-        import org.w3c.dom.Element;
-        import org.w3c.dom.Node;
-        import org.w3c.dom.NodeList;
-        import org.xml.sax.SAXException;
+import FPTS.Core.Model;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 
 import javax.xml.parsers.DocumentBuilder;
