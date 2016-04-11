724bfc6a8052794aa8a62b1baaaa925a1febffbb
Beetee14 <Beetee14@users.noreply.github.com>
2016-04-11 07:19:13 -0400

    Update NoGrowthMarketSimulation.java

--- src/FPTS/MarketSimulation/NoGrowthMarketSimulation.java
+++ src/FPTS/MarketSimulation/NoGrowthMarketSimulation.java
@@ -2,6 +2,7 @@ package FPTS.MarketSimulation;
 
 /**
  * Created by traub_000 on 3/11/2016.
+ * A no-growth algorithm doesn't change the portfolio's value
  */
 public class NoGrowthMarketSimulation extends MarketSimulationAlgorithm{
 
